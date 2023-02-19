41b1b66591879b3c8b17e45824e976d49be90ad8
HYH <969519424@qq.com>
2023-02-19 18:15:41 +0800

    doudizhushunzi

--- prefixANDprofix.c
+++ prefixANDprofix.c
@@ -5,35 +5,9 @@
 #include <stdbool.h>
 //#include<malloc.h>
 
-int ans[9];
-int used[9] = {0};
-int n, k;
-int found = 0, count = 0;
-int lenth = 0;
-void permutation() {
-    if (lenth == n) {
-        count++;
-        if (count == k) {
-            found = 1;
-            for (int i = 0; i < lenth; i++)
-                printf("%d", ans[i]);
-            printf("\n");
-        }
-        
-        return;
-    }
-    for (int i = 0; i < n && !found; i++) {
-        if (used[i]) continue;
-        used[i] = 1;
-        ans[lenth] = i + 1;
-        lenth++;
-        permutation();
-        used[i] = 0;
-        lenth--; 
-    }
-}
 
 int main(void) {
+    
     scanf("%d %d", &n, &k);
     permutation();
     system("pause");
