ec023684666b38068a0ae415906e0ca6848da1a2
HYH <969519424@qq.com>
2023-02-20 23:56:39 +0800

    start.

--- lianxudizengxulie.c
+++ lianxudizengxulie.c
@@ -15,31 +15,7 @@ int main(void) {
         getchar();
         return -1;
     }
-    char ch;
-    char preChar;
-    int start = 0;
-    int lenth = 0;
-    int ans = 0;
-    while ((ch = fgetc(fp)) != EOF) {
-        if (isdigit(ch)) {
-            if (lenth == 0){
-                lenth++;
-            }
-            else if (ch == preChar + 1 || ch == preChar) {
-                lenth++;
-            }
-            else {
-                ans = fmax(ans, lenth);
-                lenth = 1;
-            }
-            preChar = ch;
-        }
-        else {
-            lenth = 0;
-        }
-    }
-    ans = fmax(ans, lenth);
-    printf("%d\n", ans);
+    
     system("pause");
     return 0;
 }
