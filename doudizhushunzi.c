b19ba8070837c5359472af6594f34c00e1cc3eb1
HYH <969519424@qq.com>
2023-02-20 15:23:24 +0800

    success.

--- doudizhushunzi.c
+++ doudizhushunzi.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <math.h>
 //#include<malloc.h>
 
 
@@ -14,8 +15,31 @@ int main(void) {
         getchar();
         return -1;
     }
-    char c = fgetc(fp);
-    
+    char ch;
+    char preChar;
+    int start = 0;
+    int lenth = 0;
+    int ans = 0;
+    while ((ch = fgetc(fp)) != EOF) {
+        if (isdigit(ch)) {
+            if (lenth == 0){
+                lenth++;
+            }
+            else if (ch == preChar + 1 || ch == preChar) {
+                lenth++;
+            }
+            else {
+                ans = fmax(ans, lenth);
+                lenth = 1;
+            }
+            preChar = ch;
+        }
+        else {
+            lenth = 0;
+        }
+    }
+    ans = fmax(ans, lenth);
+    printf("%d\n", ans);
     system("pause");
     return 0;
 }
