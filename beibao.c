702a830a3b752a56986b9baa9c4a7492e8bf356d
HYH <969519424@qq.com>
2023-05-21 09:25:48 +0800

    commit one

--- beibao.c
+++ beibao.c
@@ -9,20 +9,64 @@
 
 int N, M;
 
+char * longestPalindrome(char * s){
+    int slenth = strlen(s);
+    int dp[slenth][slenth];
+    memset(dp, 0, sizeof(dp));
+    int i, j;
+    int start;
+    int result = 1;
+    char* ss = malloc(sizeof(char) * 1000);
+    for (i = slenth - 1; i >=0; i--)
+        for(j = i; j < slenth; j++) {
+            if (s[i] == s[j]) {
+                if ((j - i) <= 1) {
+                    dp[i][j] = 1;
+                    if (result < (j - i + 1)) {
+                        start = i;
+                        result = j - i + 1;
+                    }
+                } else if(dp[i + 1][j - 1]){
+                    if (result < (j - i + 1)) {
+                        start = i;
+                        result = j - i + 1;
+                    }
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+            }
+        }
+    for (int k = 0; k < result; k++)
+        ss[k] = s[start + k];
+    ss[result] = '\0';
+    return ss;
+}
+
+typedef struct ListNode1 ListNode;
+
+struct ListNode1 {
+    int val;
+    ListNode *next;
+};
+char str[3][30] = {
+    "http://c.biancheng.net",
+    "C语言中文网",
+    "C-Language"
+};
 
 int main(void) {
-    pid_t pid = fork();
-    if (pid == -1) {
-        printf("失败！");
+
+    char **s = str;
+    printf("%p %p %d\n", s, s + 1, sizeof(int *));
+    for(int i = 0; i<3; i++){
+        printf("str[%d]: %s\n", i, (s + i));
     }
-    else if (pid == 0) {
-        printf("I am child, my pid：%d, ppid：%d\n",getpid(),getppid());
-        sleep(5);
-    }
-    else {
-        printf("I am father, my pid：%d, ppid：%d\n",getpid(),getppid());
-        sleep(5);
-    }
+
+    char str[100];
+    float money = 123.1;
+    int m = sprintf(str, "%-08.2f", money);
+    printf("%s\n", str);
+    char* ss = longestPalindrome("babad");
+    printf("%s\n", ss);
     FILE* fp = NULL;
     fp = fopen("C:\\Users\\Administrator\\Desktop\\input.txt", "r");
     if (fp == NULL) {
